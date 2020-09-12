457cfd5bb1e7211d6e69f4b0907269ae79558f31
DishaTyagi <omgdisha@gmail.com>
2020-09-12 22:48:34 +0530

    added partials

--- src/index.js
+++ src/index.js
@@ -6,10 +6,17 @@ const hospital = require('./models/hospital');
 const test = require('./models/test');
 
 const app = express();
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
