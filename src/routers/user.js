e570611be561434f1b2cd63311ab286025082663
rajnish1999 <rajnishtiwarihrt@gmail.com>
2020-09-27 12:41:54 +0530

    added user edit option

--- src/routers/user.js
+++ src/routers/user.js
@@ -17,13 +17,37 @@ router.post('/signUp', (req, res) => {
     const user = new User(req.body);
 
     user.save().then(() => {
-        res.status(201).send(user);
+        res.redirect('/')
     }).catch((error) => {
         res.status(400).send(error);
     })
 })
 
-router.patch('/users/:id', (req, res) => {
-    
+router.get('/editUser/:id', (req, res) => {
+    const _id = req.params.id;
+    User.findById(_id).then((user) => {
+        res.render('editUser',{
+            user
+        });
+    })
 })
+
+router.post('/editUser/:id', (req, res) => {
+    const updates = Object.keys(req.body);
+    console.log(updates);
+    User.findById(req.params.id).then((user) => {
+        updates.forEach((update) => {
+            //here updates contains the keys in the form of string, and also req.body is an object, hence to access the value corresponding to given key, we have applied square bracket because the key is string and to access this keys value , i have to access usin sq. brac., if it would have been normal key, not string one, then simple dot operator would have worked
+            user[update] = req.body[update];
+        })
+        user.save().then(() => {
+            res.redirect('/');
+        }).catch((e) => {
+            console.log(e);
+        })
+    }).catch((e) => {
+        console.log(e);
+    })
+})
+
 module.exports = router;
