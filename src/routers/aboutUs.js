50254c0fa14abb7232e98e1f00b7e8cfec1798bc
rajnish1999 <rajnishtiwarihrt@gmail.com>
2020-12-26 20:12:22 +0530

    added routes for aboutUs and contactUs page

---
+++ src/routers/aboutUs.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/aboutUs', (req, res) => {
+    let departments = [];
+    Department.find({}).then((departmentsArray) => {
+        departments = departmentsArray
+        res.render('aboutUs',{
+            departments
+        });
+    }).catch((error) => {
+        console.log(error);
+    })
+})
+
+module.exports = router;
---
+++ src/routers/contactUs.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/contactUs', (req, res) => {
+    let departments = [];
+    Department.find({}).then((departmentsArray) => {
+        departments = departmentsArray
+        res.render('contactUs',{
+            departments
+        });
+    }).catch((error) => {
+        console.log(error);
+    })
+})
+
+module.exports = router;
